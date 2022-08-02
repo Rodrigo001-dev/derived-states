00a72fa7fef1daffb5331fa3c50047da90adb49b
Rodrigo <rodrigorael53@gmail.com>
2022-08-02 20:22:25 -0300

    doing it wrong

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,10 @@ interface Repo {
 
 function App() {
   const [repos, setRepos] = useState<Repo[]>([]);
+  const [filteredRepos, setFilteredRepos] = useState<Repo[]>([]);
+  const [search, setSearch] = useState('');
+
+  console.log('Renderizou');
 
   useEffect(() => {
     fetch('https://api.github.com/users/Rodrigo001-dev/repos')
@@ -14,19 +18,46 @@ function App() {
       .then(data => setRepos(data))
   }, []);
 
+  useEffect(() => {
+    // o if (search.length) vai evitar uma renderização
+    if (search.length) {
+      setFilteredRepos(repos.filter(repo => repo.name.includes(search)));
+    }
+  }, [search]);
+
   return (
     <div>
-      <input name="search" type="text" placeholder="Buscar..." />
+      <input 
+        name="search" 
+        type="text" 
+        placeholder="Buscar..."
+        onChange={e => setSearch(e.target.value)}
+        value={search}
 
-      <ul>
-        {repos.map(repo => {
-          return (
-            <li key={repo.name}>
-              {repo.name}
-            </li>
-          )
-        })}
-      </ul>
+      />
+
+      {/* se existir alguma coisa na busca */}
+      { search.length > 0 ? (
+        <ul>
+          {filteredRepos.map(repo => {
+            return (
+              <li key={repo.name}>
+                {repo.name}
+              </li>
+            )
+          })}
+        </ul>
+      ) : (
+        <ul>
+          {repos.map(repo => {
+            return (
+              <li key={repo.name}>
+                {repo.name}
+              </li>
+            )
+          })}
+        </ul>
+      ) }
     </div>
   )
 }
